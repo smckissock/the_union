3bfafe0b1e9cffb25015b9e0d735c89147d7890a
Scott McKissock <scottmckissock@gmail.com>
2022-04-23 08:11:42 -0400

    Remove unused files

--- python/delete_diffbot_batches.py
+++
@@ -1,6 +0,0 @@
-from requests import get, post
-
-
-def delete_batch(batch_name):
-    params = {'token': '3cf697d4387a105d959518bd5b682679', 'name': batch_name, 'delete': 1}
-    get('https://api.diffbot.com/v3/bulk', params=params)
